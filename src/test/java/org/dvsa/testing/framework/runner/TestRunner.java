75524564489e7ccf94f042a02a8b854c879ae075
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-03-08 10:15:06 +0000

    Adding a tear down for the tests

--- src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
+++ src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
@@ -2,12 +2,19 @@ package org.dvsa.testing.framework.runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.dvsa.testing.lib.browser.Browser;
+import org.junit.After;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber"},
-        features = {"src/test/resources/features"},
+        features = {"src/test/resources/features/email-inform-message.feature"},
         glue = {"org.dvsa.testing.framework.stepdefs"}
 )
 public class TestRunner {
+    @After
+    public void tearDown() {
+        Browser.quit();
+    }
 }
+
