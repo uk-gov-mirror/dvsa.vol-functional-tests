0ad5fb43e5b6532efead5a6574a2ce5c4ca70729
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-03-12 11:28:13 +0000

    Fix test runner

--- src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
+++ src/test/java/org/dvsa/testing/framework/runner/TestRunner.java
@@ -1,16 +1,22 @@
-package runner;
+package org.dvsa.testing.framework.runner;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.Scenario;
 import cucumber.api.junit.Cucumber;
+import org.dvsa.testing.lib.browser.Browser;
+import org.junit.After;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
 
 @RunWith(Cucumber.class)
 @CucumberOptions( plugin = {"pretty",
                             "html:target/cucumber",
                             "json:target/cucumber/json/cucumber.json",
                             "junit:target/cucumber/ci/ci.xml"},
-                  features =  "classpath:features",
-                  glue = "stepdefs",
+                  features = "src/test/resources/features",
+                  glue = "org.dvsa.testing.framework.stepdefs",
                   tags = {"not @wip or not @WIP"},
                   strict = true,
                   monochrome = true
