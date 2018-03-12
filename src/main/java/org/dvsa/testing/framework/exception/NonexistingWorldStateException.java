02812421f58a5a304fca29c0fc0d6a6d813ea342
Rodney Ruswa <rodney.ruswa@bjss.com>
2018-03-12 11:30:36 +0000

    Add constructors to NonexistingWorldStateException

--- src/main/java/org/dvsa/testing/framework/exception/NonexistingWorldStateException.java
+++ src/main/java/org/dvsa/testing/framework/exception/NonexistingWorldStateException.java
@@ -1,4 +1,18 @@
 package org.dvsa.testing.framework.exception;
 
-public class NonexistingWorldStateException {
+public class NonexistingWorldStateException extends Exception {
+    public NonexistingWorldStateException() {
+    }
+
+    public NonexistingWorldStateException(String message) {
+        super(message);
+    }
+
+    public NonexistingWorldStateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NonexistingWorldStateException(Throwable cause) {
+        super(cause);
+    }
 }
