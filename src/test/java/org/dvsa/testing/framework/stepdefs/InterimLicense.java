32dd29cb7aec83c1ae48a055fe8af03eb93b8f2f
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-03-28 16:42:34 +0100

    interimPage

--- src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
@@ -1,5 +1,7 @@
 package org.dvsa.testing.framework.stepdefs;
 
+import activesupport.number.Int;
+import activesupport.string.Str;
 import activesupport.system.Properties;
 import cucumber.api.PendingException;
 import cucumber.api.Scenario;
@@ -8,23 +10,30 @@ import javafx.scene.layout.Background;
 import org.dvsa.testing.*;
 import org.dvsa.testing.lib.Environment;
 import org.dvsa.testing.lib.URI;
-import org.dvsa.testing.lib.pages.LoginPage;
+import org.dvsa.testing.lib.pages.*;
 import org.dvsa.testing.lib.utils.ApplicationType;
 import org.dvsa.testing.lib.utils.EnvironmentType;
+import org.dvsa.testing.lib.pages.internal.*;
 
 public class InterimLicense implements En {
     public InterimLicense() {
 
         Given("^I have an interim application$", () -> {
-
-
+            Application.interimLink();
         });
+
         When("^I have zero vehicle authority on a goods variation application$", () -> {
+         InterimPage.addInterim();
+         InterimPage.enterInterimDetail(Str.randomWord(150));
+         InterimPage.startDate(12,01,2018);
+         InterimPage.endDate(12, 04, 2018);
+         InterimPage.vehicleAuthority(1);
+         InterimPage.trailerAuthority(0);
 
         });
 
         Then("^I save the application with no error$", () -> {
-
+            InterimPage.save();
         });
     }
 
--- src/test/java/org/dvsa/testing/framework/stepdefs/Utils/LoginInternalUser.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/Utils/LoginInternalUser.java
@@ -1,11 +1,13 @@
 package org.dvsa.testing.framework.stepdefs.Utils;
 
 import activesupport.system.Properties;
+import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.Environment;
 import org.dvsa.testing.lib.Login;
 import org.dvsa.testing.lib.URI;
 import org.dvsa.testing.lib.browser.Browser;
+import org.dvsa.testing.lib.pages.internal.SearchNavBar;
 import org.dvsa.testing.lib.utils.ApplicationType;
 import org.dvsa.testing.lib.utils.EnvironmentType;
 
@@ -16,13 +18,20 @@ public class LoginInternalUser implements En {
 
     public LoginInternalUser() {
 
-            Given("^I have logged in to selfserve$", () -> {
-                EnvironmentType env = Environment.enumType(Properties.get("env", true));
-                String URL = URI.build(ApplicationType.INTERNAL, env);
+        Given("^I have logged in to internal$", () -> {
+            EnvironmentType env = Environment.enumType(Properties.get("env", true));
+            String URL = URI.build(ApplicationType.INTERNAL, env);
 
-                Browser.go(URL);
+            Browser.go(URL);
 
-                Login.signIn(USER_EMAIL, USER_PASSWORD);
-            });
+            Login.signIn(USER_EMAIL, USER_PASSWORD);
+        });
+
+        And("^I have an internal application$", () -> {
+            SearchNavBar.search("h");
+            SearchNavBar.applications();
+            SearchNavBar.TEMP_LICENCE();
+        });
+
     }
 }
