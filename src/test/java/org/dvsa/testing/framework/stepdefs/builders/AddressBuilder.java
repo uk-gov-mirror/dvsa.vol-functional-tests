48942d5359840771d10897d5b5e2ecc8ea21bbc2
chideyaw <wonderson.chideya@bjss.com>
2018-04-04 15:56:06 +0100

    adding psvAPI, headers and builders

---
+++ src/test/java/org/dvsa/testing/framework/stepdefs/builders/AddressBuilder.java
@@ -0,0 +1,119 @@
+package org.dvsa.testing.framework.stepdefs.builders;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "version",
+        "addressLine1",
+        "addressLine2",
+        "town",
+        "postcode",
+        "countryCode"})
+
+public class AddressBuilder {
+
+    @JsonProperty("version")
+    private String version;
+    @JsonProperty("addressLine1")
+    private String addressLine1;
+    @JsonProperty("addressLine2")
+    private String addressLine2;
+    @JsonProperty("town")
+    private String town;
+    @JsonProperty("postcode")
+    private String postcode;
+    @JsonProperty("countryCode")
+    private String countryCode;
+
+    @JsonProperty("version")
+    public String getVersion() {
+        return version;
+    }
+
+    @JsonProperty("version")
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public AddressBuilder withVersion(String version) {
+        this.version = version;
+        return this;
+    }
+
+    @JsonProperty("addressLine1")
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public AddressBuilder withAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+        return this;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public AddressBuilder withAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+        return this;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public AddressBuilder withTown(String town) {
+        this.town = town;
+        return this;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public AddressBuilder withPostcode(String postcode) {
+        this.postcode = postcode;
+        return this;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public AddressBuilder withCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+        return this;
+    }
+
+    @Override
+    public String toString(){
+        return "version:" + getVersion() +
+        ",addressLine1:" + getAddressLine1() + ",addressLine2:" + getAddressLine2()
+                + ",town:" + getTown() + ",postcode:" + getPostcode() + ",countryCode:"+getCountryCode();
+    }
+}
