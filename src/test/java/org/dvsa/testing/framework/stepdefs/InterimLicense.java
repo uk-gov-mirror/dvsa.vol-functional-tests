909a92c256dccd67b49c352679ab0b4e8623ac22
chideyaw <wonderson.chideya@bjss.com>
2018-04-06 15:09:34 +0100

    added feature file for no dates validation feature

--- src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
@@ -1,17 +1,20 @@
 package org.dvsa.testing.framework.stepdefs;
 
+import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.internal.*;
 import org.joda.time.LocalDate;
 import org.dvsa.testing.lib.pages.BasePage;
 
 import static org.dvsa.testing.framework.stepdefs.Utils.APICreateInterimGoodsLicence.getNoOfVehiclesRequired;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
 public class InterimLicense extends BasePage implements En {
 
-    private static String errorMessage = "The interim vehicle authority cannot exceed the total vehicle authority";
+    private static String VehicleErrorMessage = "The interim vehicle authority cannot exceed the total vehicle authority";
+    private static String noDatesErrorMessage = "Value is required";
 
     public InterimLicense() {
 
@@ -24,23 +27,41 @@ public class InterimLicense extends BasePage implements En {
 
         Then("^I should get an error when i save the application$", () -> {
             InterimPage.save();
-            assertTrue(isTextPresent(errorMessage,60));
+            assertTrue(isTextPresent(VehicleErrorMessage,60));
         });
+
         When("^I have an interim vehicle authority equal to my application vehicle authority$", () -> {
             InterimPage.addInterim();
             InterimPage.startDate(LocalDate.now().getDayOfWeek(), LocalDate.now().getMonthOfYear(), LocalDate.now().getYear());
             InterimPage.endDate(LocalDate.now().plusDays(7).getDayOfWeek(), LocalDate.now().plusMonths(2).getMonthOfYear(), LocalDate.now().getYear());
             InterimPage.vehicleAuthority(getNoOfVehiclesRequired());
         });
+
         Then("^I should be able to save the application without any errors$", () -> {
             InterimPage.save();
-            assertTrue(!isTextPresent(errorMessage,60));
+            assertFalse(isTextPresent(VehicleErrorMessage,60));
         });
+
         When("^I have an interim vehicle authority less than my application vehicle authority$", () -> {
             InterimPage.addInterim();
             InterimPage.startDate(LocalDate.now().getDayOfWeek(), LocalDate.now().getMonthOfYear(), LocalDate.now().getYear());
             InterimPage.endDate(LocalDate.now().plusDays(7).getDayOfWeek(), LocalDate.now().plusMonths(2).getMonthOfYear(), LocalDate.now().getYear());
             InterimPage.vehicleAuthority(getNoOfVehiclesRequired() -1);
+        });
+
+        When("^I create an interim application with no start and end dates$", () -> {
+            InterimPage.vehicleAuthority(getNoOfVehiclesRequired());
+            InterimPage.trailerAuthority(getNoOfVehiclesRequired());
+        });
+
+        Then("^I should not error when i save the application$", () -> {
+            InterimPage.save();
+            assertFalse(isTextPresent(noDatesErrorMessage,60));
+        });
+
+        Then("^I should not error when i attempt to grant the application$", () -> {
+            InterimPage.grant();
+            assertTrue(isTextPresent(noDatesErrorMessage,60));
         });
     }
 }
