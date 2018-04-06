6d02407402cc2094543b64b2bfeaed273a45b5e8
chideyaw <wonderson.chideya@bjss.com>
2018-04-06 11:58:01 +0100

    upadted page-object version

--- src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/InterimLicense.java
@@ -1,6 +1,5 @@
 package org.dvsa.testing.framework.stepdefs;
 
-import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.internal.*;
 import org.joda.time.LocalDate;
