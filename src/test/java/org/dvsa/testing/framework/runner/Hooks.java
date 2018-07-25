435caa1aedcb9a2e36af2add8acec39110abcf81
Veena Lala <veenalala@Veenas-MacBook-Pro.local>
2018-07-25 09:50:47 +0100

    Refactored hooks class

--- src/test/java/org/dvsa/testing/framework/runner/Hooks.java
+++ src/test/java/org/dvsa/testing/framework/runner/Hooks.java
@@ -5,7 +5,8 @@ import org.dvsa.testing.lib.browser.Browser;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.time.Instant;
 
 public class Hooks {
@@ -17,18 +18,28 @@ public class Hooks {
         teardown();
     }
 
+    private void createDirectory() {
+        if (!directory.exists()) {
+            directory.mkdir();
+        } else {
+            System.out.println(directory + " folder already exists");
+        }
+    }
+
     @Attachment(value = "Screenshot on failure", type = "image/png")
     public byte[] attach() {
+        createDirectory();
         File screenshot = new File(String.format(directory + "/errorScreenShot%s.png", Instant.now().getEpochSecond()));
         try {
-            if(Browser.isInitialised()){
-            FileOutputStream screenshotStream = new FileOutputStream(screenshot);
-            byte[] bytes = ((TakesScreenshot) Browser.getDriver())
-                    .getScreenshotAs(OutputType.BYTES);
-            screenshotStream.write(bytes);
-            screenshotStream.close();
-            return bytes;
-        } } catch (Exception e) {
+            if (Browser.isInitialised()) {
+                FileOutputStream screenshotStream = new FileOutputStream(screenshot);
+                byte[] bytes = ((TakesScreenshot) Browser.getDriver())
+                        .getScreenshotAs(OutputType.BYTES);
+                screenshotStream.write(bytes);
+                screenshotStream.close();
+                return bytes;
+            }
+        } catch (Exception e) {
             System.err.println("Unable to write "
                     + screenshot.getAbsolutePath());
             e.printStackTrace();
@@ -36,7 +47,7 @@ public class Hooks {
         return null;
     }
 
-    public void teardown() {
+    private void teardown() {
         if (directory.exists()) {
             try {
                 directory.delete();
