54aaaba96e74475fc35ba6346667880fb55d890f
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-08-07 10:32:04 +0100

    refactor to use generic utils methods

--- src/test/java/org/dvsa/testing/framework/stepdefs/GoodVarIncreaseVehicle.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/GoodVarIncreaseVehicle.java
@@ -9,31 +9,16 @@ import org.dvsa.testing.lib.pages.enums.SelectorType;
 public class GoodVarIncreaseVehicle extends BasePage implements En  {
     World world = new World();
 
-    public GoodVarIncreaseVehicle() {
-        Given("^I have a valid good licence$", () -> {
-            world.genericUtils = new GenericUtils(world);
-            world.genericUtils.createApplication();
-            world.genericUtils.payFeesAndGrantLicence();
-            world.genericUtils.grantLicence().payGrantFees();
-            System.out.println("Licence: " + world.createLicence.getLicenceNumber());
-            world.genericUtils.externalUserLogin();
-            clickByLinkText(world.createLicence.getLicenceNumber());
-        });
+    public GoodVarIncreaseVehicle(World world) {
 
         When("^A selfserve user increases the vehicle authority count$", () -> {
-        clickByLinkText("Operating centres and authorisation");
-        clickByLinkText("change your licence");
-        waitAndClick("button[name='form-actions[submit]'", SelectorType.CSS);
-        waitAndClick("//*[@id=\"OperatingCentres\"]/fieldset[1]/div/div[2]/table/tbody/tr/td[1]/input", SelectorType.XPATH);
-        enterField(nameAttribute("input", "data[noOfVehiclesRequired]"),"6");
-        click(nameAttribute("button", "form-actions[submit]"));
-        click(nameAttribute("button", "form-actions[submit]"));
-        enterField(nameAttribute("input", "data[totAuthVehicles]"),"6");
-        click(nameAttribute("button", "form-actions[save]"));
+         world.genericUtils.externalUserLogin();
+         clickByLinkText(world.createLicence.getLicenceNumber());
+         world.genericUtils.changeVehicleAuth("6");
         });
 
         Then("^a status of update required should be shown next to financial evidence$", () -> {
-       isExpectedTextInElementWithin(".status.orange, .orange.overview__status", "REQUIRES ATTENTION", 10);
+         isExpectedTextInElementWithin(".status.orange, .orange.overview__status", "REQUIRES ATTENTION", 10);
         });
     }
 }
