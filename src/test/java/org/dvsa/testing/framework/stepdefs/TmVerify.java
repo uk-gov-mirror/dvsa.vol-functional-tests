9b8c29fbb821eb3cb1490732e3a529eeb1e8961c
Kishan Patel <kishan.patel@dvsa.gov.uk>
2018-09-04 14:41:22 +0100

    adding step defs for check your answers page

--- src/test/java/org/dvsa/testing/framework/stepdefs/TmVerify.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/TmVerify.java
@@ -60,6 +60,72 @@ public class TmVerify extends BasePage implements En {
         And("^the following \"([^\"]*)\" text will be displayed on the page$", (String arg0) -> {
             assertTrue(isElementPresent(arg0));
         });
+        Given("^the operator is on check your answers page$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        Then("^the correct headings, data and links should be displayed for Your details section$", () -> {
+            //TODO: This is written for the prototype please amend
+            assertTrue(isTextPresent("Your details", 30));
+            assertTrue(isTextPresent("Phil Jowett", 30));
+            assertTrue(isTextPresent("11/11/2000", 30));
+            assertTrue(isTextPresent("Nottingham", 30));
+            assertTrue(isTextPresent("Nottingham", 30));
+            assertTrue(isTextPresent("certificate attached", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Responsibilities section$", () -> {
+            assertTrue(isTextPresent("Responsibilities", 30));
+            assertTrue(isTextPresent("test", 30));
+            assertTrue(isTextPresent("test", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Hours per week section$", () -> {
+            assertTrue(isTextPresent("Hours per week", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            assertTrue(isTextPresent("8", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+
+
+        Then("^the correct headings, data and links should be displayed for Other licences section$", () -> {
+            assertTrue(isTextPresent("Other licences", 30));
+            assertTrue(isTextPresent("non added", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Additional information section$", () -> {
+            assertTrue(isTextPresent("Other licences", 30));
+            assertTrue(isTextPresent("non added", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Other employment section$", () -> {
+            assertTrue(isTextPresent("Other Employment", 30));
+            assertTrue(isTextPresent("non added", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Convictions & Penalties section$", () -> {
+            assertTrue(isTextPresent("Convictions & Penalties", 30));
+            assertTrue(isTextPresent("information added", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
+        Then("^the correct headings, data and links should be displayed for Revoked, curtailed or suspended licences section$", () -> {
+            assertTrue(isTextPresent("Revoked Curtailed or Suspended Licences", 30));
+            assertTrue(isTextPresent("information added", 30));
+            //TODO: Add link
+            assertTrue(isElementPresent(""));
+        });
     }
 
     private String confirmationPanel(String cssValue) throws IllegalBrowserException {
