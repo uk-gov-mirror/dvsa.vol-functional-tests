607c8c9897ab3fedf6432d39046373c051413300
chideyaw <wonderson.chideya@bjss.com>
2018-10-09 16:17:03 +0100

    added a test to check that signing options are not displayed when verify is switched off

--- src/test/java/org/dvsa/testing/framework/stepdefs/VerifySwitchedOff.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/VerifySwitchedOff.java
@@ -1,12 +1,14 @@
 package org.dvsa.testing.framework.stepdefs;
 
 import Injectors.World;
+import cucumber.api.PendingException;
 import cucumber.api.java8.En;
 import org.dvsa.testing.framework.Utils.Generic.GenericUtils;
 import org.dvsa.testing.lib.pages.BasePage;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class VerifySwitchedOff extends BasePage implements En {
     String flag;
@@ -21,13 +23,27 @@ public class VerifySwitchedOff extends BasePage implements En {
             }
             world.APIJourneySteps.createApplication();
         });
-        When("^I navigate to the declaration page$", () -> {
-            //update to navigate to TM declaration page
-            world.UIJourneySteps.navigateToTMReviewAndDeclarationsPage();
-        });
         Then("^Signing options are not displayed on the page$", () -> {
             assertFalse(isElementPresent("//*[@type='radio']", SelectorType.XPATH));
             assertFalse(isTextPresent("How would you like to sign the declaration?", 20));
+        });
+        And("^submit to operator button is displayed$", () -> {
+            assertTrue(isElementPresent("//*[@id='form-actions[submitToOperator]']"));
+        });
+        And("^submit to operator button is not displayed$", () -> {
+            assertTrue(isElementPresent("//*[@id='form-actions[submitToOperator]']"));
+        });
+        And("^i add a transport manager$", () -> {
+            world.UIJourneySteps.navigateToExternalUserLogin();
+            clickByLinkText(world.createLicence.getApplicationNumber());
+            world.UIJourneySteps.addExistingPersonAsTransportManager();
+        });
+        When("^the transport manager is the owner$", () -> {
+            // Write code here that turns the phrase above into concrete actions
+            throw new PendingException();
+        });
+        And("^the transport manager is not the owner$", () -> {
+            world.UIJourneySteps.updateTMDetailsAndNavigateToDeclarationsPage("Yes");
         });
     }
 }
--- src/test/resources/features/SelfServe/tm-verify-off.feature
+++ src/test/resources/features/SelfServe/tm-verify-off.feature
@@ -1,12 +1,16 @@
 Feature: Countersigning declaration page for the operator (Verify switched off)
 
-  Scenario Outline: Verify switched off check text
-    Given I have a "goods" "<Flag>" application
-    And the transport manager is the operator
-    When I navigate to the declaration page
-    Then Signing options are not displayed on the page
+  Background:
+    Given verify has been switched off
+    When I have a "goods" "<Flag>" application
+    And i add a transport manager
 
-    Examples:
-      | Flag |
-#      | GB   |
-      | NI   |
+  Scenario: Verify switched off operator same as TM
+    And the transport manager is the owner
+    Then Signing options are not displayed on the page
+    And submit to operator button is displayed
+
+  Scenario: Verify switched off operator not TM
+    And the transport manager is not the owner
+    Then Signing options are not displayed on the page
+    And submit to operator button is not displayed
