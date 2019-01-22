b392f146f6e5795964e5eb49237c93ea8c30b14f
Wonderson Chideya <wondersonchideya@gmail.com>
2019-01-22 11:58:28 +0000

    added docker files to repo

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export DISPLAY=:$DISPLAY_ID
+Xvfb :$DISPLAY_ID &
+
+if [ -z "${CUCUMBER_TAGS// }" ]
+then
+  mvn verify -Denv=dev -Dbrowser=firefox -Dwebdriver-manager=false
+else
+  mvn verify -Denv=dev -Dbrowser=firefox -Dwebdriver-manager=false -Dcucumber.options="--tags $CUCUMBER_TAGS"
+fi
