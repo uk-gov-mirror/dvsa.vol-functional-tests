4dc21cb426886f19e83dc707fdae3272f7dcb1a5
Wonderson Chideya <wondersonchideya@gmail.com>
2019-01-23 18:27:41 +0000

    added parameter for env

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ Xvfb :$DISPLAY_ID &
 
 if [ -z "${CUCUMBER_TAGS// }" ]
 then
-  mvn verify $optional_params_args -Dbrowser=firefox -Dwebdriver-manager=false
+  mvn verify -Denv=$optional_params_args -Dbrowser=firefox -Dwebdriver-manager=false
 else
-  mvn verify $optional_params_args -Dbrowser=firefox -Dwebdriver-manager=false -Dcucumber.options="--tags $CUCUMBER_TAGS"
+  mvn verify -Denv=$optional_params_args -Dbrowser=firefox -Dwebdriver-manager=false -Dcucumber.options="--tags $CUCUMBER_TAGS"
 fi
