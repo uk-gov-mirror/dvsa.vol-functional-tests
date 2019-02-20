1b5df21dd4d8fc932c616d4676db80a27b121d2a
Wonderson Chideya <wondersonchideya@gmail.com>
2019-02-20 10:16:21 +0000

    updated tags

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ Xvfb :$DISPLAY_ID &
 
 if [ -z "${CUCUMBER_TAGS// }" ]
 then
-  mvn verify -Denv=qa -Dbrowser=firefox -Dwebdriver-manager=false
+  mvn clean verify -Denv=qa -Dbrowser=firefox -Dwebdriver-manager=false
 else
-  mvn verify -Denv=qa -Dbrowser=firefox -Dwebdriver-manager=false -Dcucumber.options="--tags $CUCUMBER_TAGS"
+  mvn clean verify -Denv=qa -Dbrowser=firefox -Dwebdriver-manager=false -Dcucumber.options="--tags $CUCUMBER_TAGS"
 fi
