c81ef59d938ff2d897e98ae96691d4830a6d00cd
hares <shaun.hare@dvsa.gov.uk>
2019-04-03 15:57:38 +0000

    Update PaymentProcessing.java

--- src/test/java/org/dvsa/testing/framework/stepdefs/PaymentProcessing.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/PaymentProcessing.java
@@ -1,7 +1,6 @@
 package org.dvsa.testing.framework.stepdefs;
 
 import Injectors.World;
-import activesupport.IllegalBrowserException;
 import cucumber.api.java8.En;
 import org.dvsa.testing.lib.pages.BasePage;
 import org.dvsa.testing.lib.pages.enums.SelectorType;
