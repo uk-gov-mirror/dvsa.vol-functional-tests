3306dcb67e6e2cc3206a2c6be85e10313afd7e47
whitehousea <whitehousea@Users-MacBook-Pro.local>
2019-04-25 15:18:27 +0100

    Publish public inquiry test completed and runs fine

--- src/test/java/org/dvsa/testing/framework/stepdefs/CreateCase.java
+++ src/test/java/org/dvsa/testing/framework/stepdefs/CreateCase.java
@@ -10,6 +10,8 @@ import org.dvsa.testing.lib.pages.enums.SelectorType;
 import org.hamcrest.Matchers;
 import org.openqa.selenium.WebElement;
 
+
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class CreateCase extends BasePage implements En {
@@ -68,6 +70,7 @@ public class CreateCase extends BasePage implements En {
             response = world.updateLicence.getCaseDetails("processing/note",world.updateLicence.getCaseNoteId());
             assertThat(response.body("comment", Matchers.equalTo("case note submitted through the API")));
         });
+
         And("^i add a new public inquiry$", () -> {
             click("//*[@id='menu-licence/cases']", SelectorType.XPATH);
             clickByLinkText(Integer.toString(world.updateLicence.getCaseId()));
@@ -75,30 +78,52 @@ public class CreateCase extends BasePage implements En {
             clickByLinkText("Add Public Inquiry");
 
             // Creation of public inquiry
+            waitForTextToBePresent("Add Traffic Commissioner agreement and legislation");
             enterText("//*[@id='fields[agreedDate]_day']","21",SelectorType.XPATH);
             enterText("//*[@id='fields[agreedDate]_month']","6",SelectorType.XPATH);
             enterText("//*[@id='fields[agreedDate]_year']","2014",SelectorType.XPATH);
             selectValueFromDropDown("//*[@id='fields[agreedByTc]']",SelectorType.XPATH,"Nick Jones");
             selectValueFromDropDown("//*[@id='fields[agreedByTcRole]']",SelectorType.XPATH,"Traffic Commissioner");
             selectValueFromDropDown("//*[@id='assignedCaseworker']",SelectorType.XPATH,"ANDREW DREW");
+            click("//*[@id='fields_piTypes__chosen']/ul",SelectorType.XPATH);
             selectFirstValueInList("//*[@id='fields_piTypes__chosen']/ul");
+            click("//*[@id='fields_piTypes__chosen']/div/ul/li[1]",SelectorType.XPATH);
             selectFirstValueInList("//*[@id='fields_reasons__chosen']/ul/li/input");
+            click("//*[@id='fields_reasons__chosen']/div/ul/li[2]",SelectorType.XPATH);
             click("//*[@id='form-actions[submit]']",SelectorType.XPATH);
-
-            // Creation of hearing
-            clickByLinkText("Add hearing");
-
-
-
-
-
         });
+
         And("^i add and publish a hearing$", () -> {
+            // Creation of hearing (publishing of public inquiry)
+            waitForTextToBePresent("Add hearing");
+            clickByLinkText("Add hearing");
+            waitForTextToBePresent("Venue");
+            selectValueFromDropDown("//*[@id='venue']",SelectorType.XPATH,"Other");
+            enterText("//*[@id='venueOther']","Test",SelectorType.XPATH);
+            enterText("//*[@id='hearingDate_day']","21",SelectorType.XPATH);
+            enterText("//*[@id='hearingDate_month']","6",SelectorType.XPATH);
+            enterText("//*[@id='hearingDate_year']","2014",SelectorType.XPATH);
+            selectValueFromDropDown("//*[@id='hearingDate_hour']",SelectorType.XPATH,"16");
+            selectValueFromDropDown("//*[@id='hearingDate_minute']",SelectorType.XPATH,"00");
+            selectValueFromDropDown("//*[@id='presidingTc']",SelectorType.XPATH,"Nick Jones");
+            selectValueFromDropDown("//*[@id='presidedByRole']",SelectorType.XPATH,"Traffic Commissioner");
+            enterText("//*[@id='fields[witnesses]']","1",SelectorType.XPATH);
+            enterText("//*[@id='fields[drivers]']","1",SelectorType.XPATH);
+            click("//*[@id='form-actions[publish]']",SelectorType.XPATH);
         });
+
         Then("^the public inquiry should be published$", () -> {
+            waitForTextToBePresent("There is currently no decision");
         });
+
         And("^delete a case note$", () -> {
+            clickByLinkText("Processing");
+            clickByLinkText("Notes");
+            int numberOfNotes = 7;
+            selectRadioButtonByName("id");
+            click("//*[@id='delete']",SelectorType.XPATH);
         });
+
         Then("^the note should be deleted$", () -> {
         });
     }
